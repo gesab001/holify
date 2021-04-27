6ca530c16a9aa61b7515b678176a5f28df5c3aff
root <144000giovanni@gmail.com>
2021-04-27 00:56:04 +0000

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent  implements OnInit {
              console.log("document is hidden");
 		  }else{
           
-			       this.logUpdateData(); 
+			      
 		  }			  
 		}
 		);
